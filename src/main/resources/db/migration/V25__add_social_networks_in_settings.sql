e0bf7195ab8f0afa80ea040a1fb1a972247034cd
milan_gataric_naovis <milan.gataric@naovis.com>
2026-06-06 18:55:20 +0200

    add social network icons

---
+++ src/main/resources/db/migration/V25__add_social_networks_in_settings.sql
@@ -0,0 +1,7 @@
+INSERT INTO store_settings (key, value, label) VALUES
+  ('social_instagram', '', 'Instagram URL'),
+  ('social_facebook', '', 'Facebook URL'),
+  ('social_x', '', 'X (Twitter) URL'),
+  ('social_threads', '', 'Threads URL'),
+  ('social_tiktok', '', 'TikTok URL'),
+  ('social_youtube', '', 'YouTube URL');
