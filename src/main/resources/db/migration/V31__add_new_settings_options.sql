4f9f7c0fb6cc64dcbe17320b210f6b14967cabfd
milan_gataric_naovis <milan.gataric@naovis.com>
2026-06-14 16:51:50 +0200

    add contact page, add new setting options

---
+++ src/main/resources/db/migration/V31__add_new_settings_options.sql
@@ -0,0 +1,4 @@
+INSERT INTO store_settings (key, value, label) VALUES
+  ('contact_enabled', 'true', 'Contact Page'),
+  ('multilanguage_enabled', 'true', 'Multi-language'),
+  ('default_language', 'en', 'Default Language');
