1e7cb49eac17b789e14d74d5599a3a6d7a485390
milan_gataric_naovis <milan.gataric@naovis.com>
2026-06-29 22:00:01 +0200

    add sitemap

--- src/main/java/rs/nopressurewear/controller/SitemapController.java
+++ src/main/java/rs/nopressurewear/controller/SitemapController.java
@@ -1,5 +1,6 @@
 package rs.nopressurewear.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import rs.nopressurewear.repository.ProductRepository;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class SitemapController {
 
     private final ProductRepository productRepository;
@@ -18,28 +20,32 @@ public class SitemapController {
     @Value("${app.frontend-url}")
     private String siteUrl;
 
-    public SitemapController(ProductRepository productRepository, CategoryRepository categoryRepository) {
-        this.productRepository = productRepository;
-        this.categoryRepository = categoryRepository;
-    }
-
     @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
     public String sitemap() {
         StringBuilder sb = new StringBuilder();
         sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         sb.append("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
 
-        for (String path : List.of("", "/products", "/contact", "/privacy-policy", "/terms", "/returns", "/imprint")) {
-            sb.append("<url><loc>").append(siteUrl).append(path).append("</loc></url>");
-        }
+        List.of("", "/products", "/contact", "/privacy-policy", "/terms", "/returns", "/imprint")
+            .forEach(path -> appendUrl(sb, siteUrl + path));
 
-        productRepository.findByIsActiveTrueOrderByIdAsc().forEach(p ->
-            sb.append("<url><loc>").append(siteUrl).append("/products/").append(p.getId()).append("</loc></url>"));
+        productRepository.findByIsActiveTrueOrderByIdAsc()
+            .forEach(p -> appendUrl(sb, siteUrl + "/products/" + p.getId()));
 
-        categoryRepository.findByIsActiveTrueOrderByNameAsc().forEach(c ->
-            sb.append("<url><loc>").append(siteUrl).append("/products?categoryId=").append(c.getId()).append("</loc></url>"));
+        categoryRepository.findByIsActiveTrueOrderByNameAsc()
+            .forEach(c -> appendUrl(sb, siteUrl + "/products?categoryId=" + c.getId()));
 
         sb.append("</urlset>");
         return sb.toString();
+    }
+
+    private void appendUrl(StringBuilder sb, String loc) {
+        sb.append("<url><loc>").append(escapeXml(loc)).append("</loc></url>");
+    }
+
+    private String escapeXml(String s) {
+        return s.replace("&", "&amp;")
+                .replace("<", "&lt;")
+                .replace(">", "&gt;");
     }
 }
